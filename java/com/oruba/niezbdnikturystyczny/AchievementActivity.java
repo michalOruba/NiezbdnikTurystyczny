99c386e9050ba868874d8503203dc3785940f4e2
michalOruba <michaloruba@wp.pl>
2018-08-28 21:50:36 +0200

    Initial commit

---
+++ java/com/oruba/niezbdnikturystyczny/AchievementActivity.java
@@ -0,0 +1,13 @@
+package com.oruba.niezbdnikturystyczny;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class AchievementActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.achievement_layout);
+    }
+}
---
+++ java/com/oruba/niezbdnikturystyczny/MainActivity.java
@@ -0,0 +1,58 @@
+package com.oruba.niezbdnikturystyczny;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton menu_navigation, menu_help, menu_issue, menu_achievement;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        menu_navigation = findViewById(R.id.menu_navigation);
+        menu_help = findViewById(R.id.menu_help);
+        menu_issue = findViewById(R.id.menu_issue);
+        menu_achievement = findViewById(R.id.menu_achievement);
+
+        menu_navigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent navigationIntent = new Intent(MainActivity.this, NavigationActivity.class);
+                startActivity(navigationIntent);
+            }
+        });
+
+        menu_help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent navigationIntent = new Intent(MainActivity.this, HelpActivity.class);
+                startActivity(navigationIntent);
+            }
+        });
+
+        menu_issue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent navigationIntent = new Intent(MainActivity.this, IssueActivity.class);
+                startActivity(navigationIntent);
+            }
+        });
+
+        menu_achievement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent navigationIntent = new Intent(MainActivity.this, AchievementActivity.class);
+                startActivity(navigationIntent);
+            }
+        });
+
+
+    }
+
+
+}
