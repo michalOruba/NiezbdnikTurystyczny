742d185c00ff5ac01aa84530704418dc49b9c9ae
michalOruba <michaloruba@wp.pl>
2018-08-30 22:05:55 +0200

    1) Navigation Item Adapter has been created. 2) In Navigation List items have appeared. 3) Some visual changes in xml files.

--- java/com/oruba/niezbdnikturystyczny/Navigation1Fragment.java
+++ java/com/oruba/niezbdnikturystyczny/Navigation1Fragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Navigation1Fragment extends Fragment {
 
@@ -14,6 +17,38 @@ public class Navigation1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.navigation1_fragment,container,false);
 
+        final ArrayList<NavigationItem> hills = new ArrayList<NavigationItem>();
+
+        hills.add(new NavigationItem("Rysy", 2499, R.drawable.rysy));
+        hills.add(new NavigationItem("Rysy", 2499, R.drawable.rysy));
+        hills.add(new NavigationItem("Rysy", 2499, R.drawable.rysy));
+        hills.add(new NavigationItem("Rysy", 2499, R.drawable.rysy));
+        hills.add(new NavigationItem("Rysy", 2499, R.drawable.rysy));
+        hills.add(new NavigationItem("Rysy", 2499, R.drawable.rysy));
+        hills.add(new NavigationItem("Rysy", 2499, R.drawable.rysy));
+
+        NavigationItemAdapter adapter = new NavigationItemAdapter(getActivity(), hills);
+
+        ListView listView = (ListView) view.findViewById(R.id.navigation_list);
+
+        listView.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
+
+
         return view;
     }
+
+    public Navigation1Fragment() {
+
+    }
+
+
 }
--- java/com/oruba/niezbdnikturystyczny/NavigationItemAdapter.java
+++ java/com/oruba/niezbdnikturystyczny/NavigationItemAdapter.java
@@ -1,8 +1,17 @@
 package com.oruba.niezbdnikturystyczny;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -11,5 +20,31 @@ public class NavigationItemAdapter extends ArrayAdapter<NavigationItem> {
 
     public NavigationItemAdapter(@NonNull Activity context, ArrayList<NavigationItem> items) {
         super(context,0, items);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+        NavigationItem currentItem = getItem(position);
+
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.navigation_list_item, parent, false);
+        }
+
+        System.out.println(R.string.MASLevel);
+        TextView navigationHillName = (TextView) listItemView.findViewById(R.id.navigation_hill_name);
+        navigationHillName.setText(currentItem.getmHillName());
+
+        TextView navigationHillHeight = (TextView) listItemView.findViewById(R.id.navigation_hill_height);
+        navigationHillHeight.setText(String.valueOf(currentItem.getmHillHeight()) + getContext().getString(R.string.MASLevel));
+
+        ImageView navigationImageID = (ImageView) listItemView.findViewById(R.id.navigation_image);
+        navigationImageID.setImageResource(currentItem.getmImageResourceId());
+        navigationImageID.setVisibility(View.VISIBLE);
+
+        return listItemView;
     }
 }
